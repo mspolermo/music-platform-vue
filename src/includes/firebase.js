278dc2069242e34c690b530dc7037cd63f9da881
mspolermo <mspolermo@gmail.com>
2024-08-14 19:02:02 +0500

    firebase connected

---
+++ src/includes/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBnAUBbnSmBfIZ65XFqmIB8UJiL9dfaB5k',
+  authDomain: 'music-8f3c5.firebaseapp.com',
+  projectId: 'music-8f3c5',
+  storageBucket: 'music-8f3c5.appspot.com',
+  appId: '1:326461175146:web:c13953ef34ce6e0e572113'
+}
+
+export default firebase.initializeApp(firebaseConfig)
