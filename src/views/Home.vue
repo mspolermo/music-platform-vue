0d17ba854d5eae6421c7d4b9e1781540b72d71d0
mspolermo <mspolermo@gmail.com>
2024-09-03 18:53:30 +0500

    infinite scroll added, dynamic parameter and song page added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -49,7 +49,9 @@ export default {
   },
   data() {
     return {
-      songs: []
+      songs: [],
+      maxPerPage: 25,
+      pendingRequest: false
     }
   },
   async created() {
@@ -67,11 +69,28 @@ export default {
       const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
 
       if (bottomOfWindow) {
-        console.log('Bottom of window')
+        this.getSongs()
       }
     },
     async getSongs() {
-      const snapshots = await songsCollection.get()
+      if (this.pendingRequest) {
+        return
+      }
+
+      this.pendingRequest = true
+
+      let snapshots
+
+      if (this.songs.length) {
+        const lastDoc = await songsCollection.doc(this.songs[this.songs.length - 1].docID).get()
+        snapshots = await songsCollection
+          .orderBy('modified_name')
+          .startAfter(lastDoc)
+          .limit(this.maxPerPage)
+          .get()
+      } else {
+        snapshots = await songsCollection.orderBy('modified_name').limit(this.maxPerPage).get()
+      }
 
       snapshots.forEach((document) => {
         this.songs.push({
@@ -79,6 +98,8 @@ export default {
           ...document.data()
         })
       })
+
+      this.pendingRequest = false
     }
   }
 }
