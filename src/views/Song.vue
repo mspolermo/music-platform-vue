bd5e07a18805715657cd628dccc119ffd2f2bda5
mspolermo <mspolermo@gmail.com>
2024-09-07 13:36:33 +0500

    Query params using added

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -25,7 +25,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -124,6 +124,10 @@ export default {
       return
     }
 
+    const { sort } = this.$route.query
+
+    this.sort = sort === '1' || sort === '2' ? sort : '1'
+
     this.song = docSnapshot.data()
     this.getComments()
   },
@@ -144,6 +148,11 @@ export default {
 
       await commentsCollection.add(comment)
 
+      this.song.comment_count += 1
+      await songsCollection
+        .doc(this.$route.params.id)
+        .update({ comment_count: this.song.comment_count })
+
       this.getComments()
 
       this.comment_in_submission = false
@@ -158,6 +167,19 @@ export default {
       this.comments = []
 
       snapshots.forEach((doc) => this.comments.push({ docID: doc.id, ...doc.data() }))
+    }
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return
+      }
+
+      this.$router.push({
+        query: {
+          sort: newVal
+        }
+      })
     }
   }
 }
