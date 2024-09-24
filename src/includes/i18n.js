8604519aa09658d022887f72e10fc7e9e1888856
mspolermo <mspolermo@gmail.com>
2024-09-24 18:43:44 +0500

    formatting and pluralization rules for ru added

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -2,9 +2,29 @@ import { createI18n } from 'vue-i18n'
 import en from '@/locales/en.json'
 import ru from '@/locales/ru.json'
 
+function customRule(choice, choicesLength, orgRule) {
+  if (choice === 0) {
+    return 0
+  }
+
+  const teen = choice > 10 && choice < 20
+  const endsWithOne = choice % 10 === 1
+  if (!teen && endsWithOne) {
+    return 1
+  }
+  if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+    return 2
+  }
+
+  return choicesLength < 4 ? 2 : 3
+}
+
 export default createI18n({
   locale: 'ru',
   fallbackLocale: 'en',
+  pluralizationRules: {
+    ru: customRule
+  },
   messages: {
     en,
     ru
