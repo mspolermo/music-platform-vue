6b25f7110cc4e2e602058a0c32446a08776c09c9
mspolermo <mspolermo@gmail.com>
2024-10-02 18:23:59 +0500

    offline support with service workers

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import VeeValidatePlugin from './includes/validation.js'
 import { auth } from './includes/firebase.js'
 import Icon from './directives/icon.js'
 import i18n from './includes/i18n.js'
+import { registerSW } from 'virtual:pwa-register'
 
 import './assets/base.css'
 import './assets/main.css'
+
+registerSW({ immediate: true })
 
 let app
 auth.onAuthStateChanged(() => {
