a94529fd675e7ec82b1c63d4823d8a345c719bd6
mspolermo <mspolermo@gmail.com>
2024-10-06 10:57:58 +0500

    pwa changes

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -63,6 +63,18 @@ export default {
           return
         }
 
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: 'bg-red-400',
+            icon: 'fas fa-times',
+            text_class: 'text-red-400'
+          })
+          return
+        }
+
         const storageRef = storage.ref() //storageBucket
         const songsRef = storageRef.child(`songs/${file.name}`) //storageBucket/songs/example.mp3
         const task = songsRef.put(file)
